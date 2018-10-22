cff2e69f612dd75a811fa082f01ee9664c657b0b
aaaassssqqqq <42968162+aaaassssqqqq@users.noreply.github.com>
2018-10-22 21:19:29 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,86 +1,16 @@
 const Discord = require("discord.js");
-const client = new Discord.Client();
-var prefix = "$";
-client.on('message', message => {
+const client = new Discord.Client();});
 
-              if(!message.channel.guild) return;
-
-    var prefix = "$";
-
-    if(message.content.startsWith('$bc')) {
-
-    if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
-
-  if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
-
-    let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
-
-
-    let request = `Requested By ${message.author.username}`;
-
-    if (!args) return message.reply('**اكتب شي لي ارسال البرودكاست**');message.channel.send(`**هل أنت متأكد من إرسالك البرودكاست؟ \nمحتوى البرودكاست:** \` ${args}\``).then(msg => {
-
-    msg.react('✅')
-
-    .then(() => msg.react('❌'))
-
-    .then(() =>msg.react('✅'))
-
-    let reaction1Filter = (reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id;
-
-    let reaction2Filter = (reaction, user) => reaction.emoji.name === '❌' && user.id === message.author.id;
-
-       let reaction1 = msg.createReactionCollector(reaction1Filter, { time: 12000 });
-
-    let reaction2 = msg.createReactionCollector(reaction2Filter, { time: 12000 });
-
-    reaction1.on("collect", r => {
-
-    message.channel.send(`☑ | Done ... The Broadcast Message Has Been Sent For ${message.guild.members.size} Members`).then(m => m.delete(5000));
-
-    message.guild.members.forEach(m => {
-
-    var bc = new
-
-       Discord.RichEmbed()
-
-       .setColor('RANDOM')
-
-         .setTitle('Click here to join server')
-      
-       .setURL('https://discord.gg/ujnTphe')
-
-       .addField('Server', message.guild.name)
-
-       .addField('Sender', message.author.username)
-
-       .addField('Message', args)
-
-    m.send({ embed: bc })
-
-    msg.delete();
-
-    })
-
-    })
-
-    reaction2.on("collect", r => {
-
-    message.channel.send(`**Broadcast Canceled.**`).then(m => m.delete(5000));
-
-    msg.delete();
-
-    })
-
-    })
-
-
-client.on("ready", () => {
-    client.user.setGame("on " + client.users.size + " users")
-}).on("guildCreate", () => {
-    client.user.setGame("on " + client.users.size + " users")
-}).on("guildDelete", () => {
-    client.user.setGame("on " + client.users.size + " users")
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : i1Suhaib`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`By:Dexter|$help`,"http://twitch.tv/S-F")
+client.user.setStatus("dnd")
+});
 })
 
 client.login(process.env.BOT_TOKEN);

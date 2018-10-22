42a1d4998830129c8da826ab299825cb4c3b0386
aaaassssqqqq <42968162+aaaassssqqqq@users.noreply.github.com>
2018-10-22 20:57:03 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,146 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+var prefix = "$";
+client.on('message', message => {
+
+              if(!message.channel.guild) return;
+
+    var prefix = "$";
+
+    if(message.content.startsWith('$bc')) {
+
+    if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
+
+  if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
+
+    let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
+
+
+    let request = `Requested By ${message.author.username}`;
+
+    if (!args) return message.reply('**اكتب شي لي ارسال البرودكاست**');message.channel.send(`**هل أنت متأكد من إرسالك البرودكاست؟ \nمحتوى البرودكاست:** \` ${args}\``).then(msg => {
+
+    msg.react('✅')
+
+    .then(() => msg.react('❌'))
+
+    .then(() =>msg.react('✅'))
+
+    let reaction1Filter = (reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id;
+
+    let reaction2Filter = (reaction, user) => reaction.emoji.name === '❌' && user.id === message.author.id;
+
+       let reaction1 = msg.createReactionCollector(reaction1Filter, { time: 12000 });
+
+    let reaction2 = msg.createReactionCollector(reaction2Filter, { time: 12000 });
+
+    reaction1.on("collect", r => {
+
+    message.channel.send(`☑ | Done ... The Broadcast Message Has Been Sent For ${message.guild.members.size} Members`).then(m => m.delete(5000));
+
+    message.guild.members.forEach(m => {
+
+    var bc = new
+
+       Discord.RichEmbed()
+
+       .setColor('RANDOM')
+
+         .setTitle('Click here to join server')
+      
+       .setURL('https://discord.gg/ujnTphe')
+
+       .addField('Server', message.guild.name)
+
+       .addField('Sender', message.author.username)
+
+       .addField('Message', args)
+
+    m.send({ embed: bc })
+
+    msg.delete();
+
+    })
+
+    })
+
+    reaction2.on("collect", r => {
+
+    message.channel.send(`**Broadcast Canceled.**`).then(m => m.delete(5000));
+
+    msg.delete();
+
+    })
+
+    })
+
+    }
+
+
+client.on('guildMemberRemove', Sal => { //By Salto7#4595
+  var embed = new Discord.RichEmbed()
+  .setAuthor(Sal.user.username, Sal.user.avatarURL)
+  .setThumbnail(Sal.user.avatarURL)
+  .setImage('https://previews.123rf.com/images/dxinerz/dxinerz1509/dxinerz150901337/45612790-bye-goodbye-icon.jpg') //هنا حط الصوره الي تبيها
+  .setTitle('خرج عضو')
+  .setDescription('الله معك مع السلامه')
+  .addField('``ايدي العضو``:',"" +  Sal.user.id, true)
+  .addField('``تاق العضو``', Sal.user.discriminator, true)
+  .addField('``تم الانشاء في``', Sal.user.createdAt, true)
+  .addField(' 👤 الان ',`**[ ${Sal.guild.memberCount} ]**`,true)
+  .setColor('RED')
+  .setFooter(Sal.guild.name, Sal.guild.iconURL, true)
+  var channel =Sal.guild.channels.find('name', 'out') //the-space
+  if (!channel) return;
+  channel.send({embed : embed});
+  });
+    });  
+  
+
+client.on("message", message => {
+             
+     if(!message.channel.guild) return;
+
+ if (message.content === "$help") {
+   message.react("😜")
+  const embed = new Discord.RichEmbed()
+      .setColor("RANDOM")
+      .addField("$bc الرسالة", The Space)
+  message.channel.sendMessage({embed});
+
+ }
+});
+
+lient.on('message', message => {
+    if (message.content.startsWith("رابط")) {
+
+  message.channel.createInvite({
+        thing: true,
+        maxUses: 2,
+        maxAge: 86400
+    }).then(invite =>
+      message.author.sendMessage(invite.url)
+    )
+  message.channel.send("**تم ارسال الرابط برسالة خاصة**")
+
+message.author.send(`**مدة الرابط : يـوم
+عدد استخدامات الرابط : 2**`)
+
+
+    }
+});
+
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : i1Suhaib`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`By:Dexter|$help`,"http://twitch.tv/S-F")
+client.user.setStatus("dnd")
+});
+
+
+
+client.login("NDM4MDcyNzgyNTI2NjExNDU2.DqtxQg.ruJYPdlwT9EsMzDwoahWjWt33RI");
